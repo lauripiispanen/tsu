842e865641ab1fa8a8ed5bf96b5fcf0d4ef7856c
Riku Hurmalainen <riku.hurmalainen@reaktor.fi>
2013-11-29 01:52:49 +0200

    Rename package. Use ScalaTest.

--- src/main/scala/tsu/package.scala
+++ src/main/scala/tsu/package.scala
@@ -1,7 +1,7 @@
 import com.fasterxml.jackson.core.{JsonToken, JsonParser, JsonFactory}
-import java.io.Reader
+import scala.io.Source
 
-package object jsonstream {
+package object tsu {
 
   abstract class JsonValue
 
@@ -19,13 +19,17 @@ package object jsonstream {
 
   case object JsonNull extends JsonValue
 
-  def parse(is: Reader) = {
+  def parse(source: Source): JsonValue = {
+    parse(new SourceReader(source))
+  }
+
+  def parse(is: java.io.Reader): JsonValue = {
     val parser: JsonParser = new JsonFactory().createParser(is)
 
     def parseJsonValue(token: JsonToken): JsonValue = {
       token match {
-        case JsonToken.START_OBJECT => JsonObject(parseObjectFields)
-        case JsonToken.START_ARRAY => JsonArray(parseArrayValues)
+        case JsonToken.START_OBJECT => JsonObject(parseObject)
+        case JsonToken.START_ARRAY => JsonArray(parseArray)
         case JsonToken.VALUE_STRING => JsonString(parser.getText)
         case JsonToken.VALUE_NUMBER_INT => JsonNumber(parser.getDecimalValue)
         case JsonToken.VALUE_NUMBER_FLOAT => JsonNumber(parser.getDecimalValue)
@@ -36,24 +40,24 @@ package object jsonstream {
       }
     }
     
-    def parseObjectFields: Stream[JsonField] = {
+    def parseObject: Stream[JsonField] = {
       parser.nextToken match {
         case JsonToken.FIELD_NAME => {
           val key = parser.getText
           val jsonValue: JsonValue = parseJsonValue(parser.nextToken)
-          JsonField(key, jsonValue) #:: { traverse(jsonValue); parseObjectFields }
+          JsonField(key, jsonValue) #:: { traverse(jsonValue); parseObject }
         }
         case JsonToken.END_OBJECT => Stream()
         case t => throw new IllegalStateException(t.toString)
       }
     }
 
-    def parseArrayValues: Stream[JsonValue] = {
+    def parseArray: Stream[JsonValue] = {
       parser.nextToken match {
         case JsonToken.END_ARRAY => Stream()
         case t => {
           val jsonValue = parseJsonValue(t)
-          jsonValue #:: { traverse(jsonValue); parseArrayValues }
+          jsonValue #:: { traverse(jsonValue); parseArray }
         }
       }
     }
@@ -65,5 +69,22 @@ package object jsonstream {
     case JsonObject(fields) => fields.foreach(f => traverse(f.value))
     case JsonArray(values) => values.foreach(traverse)
     case _ => Unit
+  }
+
+  private class SourceReader(source: Source) extends java.io.Reader {
+    def close = source.close
+
+    def read(buf: Array[Char], offset: Int, len: Int): Int = {
+      Stream.range(offset, offset + len)
+        .map((idx: Int) => {
+        if(source.hasNext)
+          (idx, Some(source.next))
+        else
+          (idx, None)
+      })
+        .foldLeft(0)((len: Int, last: (Int, Option[Char])) => {
+        last._2.map(buf.update(last._1, _)).map(_ => len + 1).getOrElse(len)
+      })
+    }
   }
 }
---
+++ src/test/scala/tsu/Test.scala
@@ -0,0 +1,19 @@
+package tsu
+
+import org.scalatest._
+import java.io.InputStream
+import scala.io.Source
+
+class Test extends FlatSpec with Matchers {
+
+  "Parser" should "not consume more input than necessary for requested data" in {
+    val inputStream: InputStream = getClass.getResourceAsStream("/test_first_two_objects.json")
+    val value = parse(Source.fromInputStream(inputStream, "UTF-8"))
+    value match {
+      case JsonArray(fields) => {
+        fields.take(2).foreach(println)
+      }
+    }
+
+  }
+}
